addb0ff298a83450c99938b446d22dd55a961fc1
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-02-08 15:41:55 -0500

    I am done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,4 +1,6 @@
 package exercise1;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
@@ -11,8 +13,41 @@ package exercise1;
 public class CardTrick {
     
     public static void main(String[] args) {
+      
         
         Card[] hand = new Card[7];
+        Card[] hand = new Card[7];
+        hand[0]= new Card();
+        hand[0].setValue(1);
+        hand[0].setValue("Hearts");        
+        hand[1]= new Card();
+        hand[1].setValue(2);
+        hand[1].setValue("Clubs");        
+        hand[2]= new Card();
+        hand[2].setValue(3);
+        hand[2].setValue("Diomonds");
+        hand[3]= new Card();
+        hand[3].setValue(4);
+        hand[3].setValue("Spades");
+        hand[4]= new Card();
+        hand[4].setValue(5);
+        hand[4].setValue("Clubs");
+        hand[5]= new Card();
+        hand[5].setValue(12);
+        hand[5].setValue("Hearts");
+        hand[6]= new Card();
+        hand[6].setValue(11);
+        hand[6].setValue("Hearts");
+       
+        Scanner input = new Scanner(System.in);
+        int userValue =input.nextInt();
+        String userSuit= input.nextLine();
+        for(int i =0;i<hand.length;i++){
+            if(hand[i].getValue()==userValue &&
+               hand[i].getSuit().equals(userSuit)){
+                printInfo();
+            }//end of if
+        }//end of for
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
